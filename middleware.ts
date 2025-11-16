1fa62d4dde633e92a16fe82189dd8f071a066ea1
quefep <88093506+M1tsumi@users.noreply.github.com>
2025-11-16 13:12:23 -0500

    Add redirect for easy discord invite

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,10 @@ export function middleware(req: NextRequest) {
   const url = req.nextUrl;
   const host = req.headers.get('host') || '';
 
+  if (host === 'quefep.uk.invite') {
+    return NextResponse.redirect('https://discord.com/invite/6nS2KqxQtj');
+  }
+
   if (host.startsWith('swiftdisc.')) {
     if (!url.pathname.startsWith('/swiftdisc')) {
       url.pathname = '/swiftdisc' + (url.pathname === '/' ? '' : url.pathname);
